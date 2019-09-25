6b46729b0f67e7f2d95287acb25df9fcb04e0a60
Ivan Rendon <ivansito87@gmail.com>
2019-09-25 11:30:08 -0400

    Updates

---
+++ precache-manifest.50f2c2efc862f8a9eb5d1bfec8dd891f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "01909aee6241e13267eb",
+    "url": "/React_Redux_App/static/css/main.2a8d1271.chunk.css"
+  },
+  {
+    "revision": "01909aee6241e13267eb",
+    "url": "/React_Redux_App/static/js/main.66823b72.chunk.js"
+  },
+  {
+    "revision": "b0da6df9465cdd5a1477",
+    "url": "/React_Redux_App/static/js/runtime~main.588b4bcc.js"
+  },
+  {
+    "revision": "e3dee9192c2d87ba09bb",
+    "url": "/React_Redux_App/static/js/2.d2f5fdc9.chunk.js"
+  },
+  {
+    "revision": "787b6662eb1b399c1172f7411d122574",
+    "url": "/React_Redux_App/static/media/ivan-react.787b6662.png"
+  },
+  {
+    "revision": "f38ddea9dedadea03c5d43c596dac13f",
+    "url": "/React_Redux_App/static/media/argon-react.f38ddea9.png"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/React_Redux_App/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/React_Redux_App/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "028047490fd9125a94a228e3c8ed2c34",
+    "url": "/React_Redux_App/static/media/ivan.02804749.JPG"
+  },
+  {
+    "revision": "c87c1d3a1b27538d4a5dd777e5ed341e",
+    "url": "/React_Redux_App/index.html"
+  }
+];
